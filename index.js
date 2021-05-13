af34568a67a8fb327aef2593234822e36290b40e
Mihir Chaturvedi <plibither8@gmail.com>
2021-05-13 11:42:49 +0530

    style: lint using Prettier

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-require('dotenv').config({path: __dirname + '/.env'});
-const fetch = require('node-fetch');
-const {writeFile} = require('fs/promises');
-const {createHash} = require('crypto');
-const path = require('path');
-const {launchBrowserAndLogin, getLatestStatement, logoutAndCloseBrowser} = require('./statement');
-const config = require(path.resolve('./', process.argv[2] ?? 'config.json'));
+require("dotenv").config({ path: __dirname + "/.env" });
+const fetch = require("node-fetch");
+const { writeFile } = require("fs/promises");
+const { createHash } = require("crypto");
+const path = require("path");
+const {
+  launchBrowserAndLogin,
+  getLatestStatement,
+  logoutAndCloseBrowser,
+} = require("./statement");
+const config = require(path.resolve("./", process.argv[2] ?? "config.json"));
 
-const hash = ({description, id}) => createHash('md5').update(`${description}${id}`).digest('hex');
+const hash = ({ description, id }) =>
+  createHash("md5").update(`${description}${id}`).digest("hex");
 
 let state;
 try {
-  state = require('./state.json');
+  state = require("./state.json");
 } catch (err) {
   state = {};
 }
 
-const {TG_BOT_NAME, TG_BOT_SECRET} = process.env;
-const formatCurrency = num => `₹ \`${Number(num).toLocaleString('en-IN')}\``;
+const { TG_BOT_NAME, TG_BOT_SECRET } = process.env;
+const formatCurrency = (num) => `₹ \`${Number(num).toLocaleString("en-IN")}\``;
 
 async function notify(transaction, account) {
-  const {id, description, date, withdrawal, deposit, closingBalance} = transaction;
+  const { id, description, date, withdrawal, deposit, closingBalance } =
+    transaction;
   const debit = deposit === 0;
 
-  const message = `*💰${debit ? '🔴 DEBIT' : '🟢 CREDIT'} @ ${account}*
+  const message = `*💰${debit ? "🔴 DEBIT" : "🟢 CREDIT"} @ ${account}*
 
-*Amount*: ${debit ? `- ${formatCurrency(withdrawal)}` : `+ ${formatCurrency(deposit)}`}
+*Amount*: ${
+    debit ? `- ${formatCurrency(withdrawal)}` : `+ ${formatCurrency(deposit)}`
+  }
 *Description*: \`${description}\`
 
 *Time*: \`${new Date().toTimeString().slice(0, 5)}\`
@@ -34,47 +42,53 @@ async function notify(transaction, account) {
 *Closing balance*: ${formatCurrency(closingBalance)}`;
 
   await fetch(`https://tg.mihir.ch/${TG_BOT_NAME}`, {
-    method: 'POST',
-    body: JSON.stringify({text: message, secret: TG_BOT_SECRET}),
-    headers: {'Content-Type': 'application/json'}
-  })
+    method: "POST",
+    body: JSON.stringify({ text: message, secret: TG_BOT_SECRET }),
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
 async function main() {
   const [browser, page] = (await launchBrowserAndLogin(config)) || [];
   if (!browser || !page) {
-    console.log('An error while logging in, try again later or fix the code!');
+    console.log("An error while logging in, try again later or fix the code!");
     return process.exit();
   }
   for (let index = 0; index < config.accounts.length; index++) {
     const account = config.accounts[index];
-    console.log('\033[1mRunning on', account, '\033[0m');
-    console.log('Getting the latest statement...');
-    const {balance, transactions} = await getLatestStatement(config, page, index);
+    console.log("\033[1mRunning on", account, "\033[0m");
+    console.log("Getting the latest statement...");
+    const { balance, transactions } = await getLatestStatement(
+      config,
+      page,
+      index
+    );
     if (!balance || !transactions) {
-      console.log('An error occured somehow, try again later or fix the code!');
+      console.log("An error occured somehow, try again later or fix the code!");
       return process.exit();
-    };
-    console.log('Received statement!');
+    }
+    console.log("Received statement!");
     const pendingTransactions = [];
     const lastTransactionHash = state[account];
     if (lastTransactionHash) {
       for (const transaction of transactions) {
         if (lastTransactionHash !== hash(transaction)) {
           pendingTransactions.push(transaction);
-        }
-        else break;
+        } else break;
       }
     }
-    !pendingTransactions.length && console.log('Nothing to notify');
+    !pendingTransactions.length && console.log("Nothing to notify");
     pendingTransactions.reverse().forEach(async (transaction, index) => {
-      console.log('Notifying...', `[${index + 1} / ${pendingTransactions.length}]`);
+      console.log(
+        "Notifying...",
+        `[${index + 1} / ${pendingTransactions.length}]`
+      );
       await notify(transaction, account);
     });
-    console.log('Updating state file...');
+    console.log("Updating state file...");
     state[account] = hash(transactions[0]);
-    await writeFile(__dirname + '/state.json', JSON.stringify(state));
-    console.log('Everything done! Cheers :)');
+    await writeFile(__dirname + "/state.json", JSON.stringify(state));
+    console.log("Everything done! Cheers :)");
   }
   await logoutAndCloseBrowser(browser, page);
 }
--- statement.js
+++ statement.js
@@ -1,16 +1,16 @@
-const pptr = require('puppeteer');
+const pptr = require("puppeteer");
 
 /** Utils */
-const wait = ms => new Promise(res => setTimeout(res, ms));
-const innerText = element => element.evaluate(el => el.innerText.trim());
-const amountToDecimal = str => Number(str.trim().replace(/,/g, ''));
+const wait = (ms) => new Promise((res) => setTimeout(res, ms));
+const innerText = (element) => element.evaluate((el) => el.innerText.trim());
+const amountToDecimal = (str) => Number(str.trim().replace(/,/g, ""));
 
 /** Constants */
-const HDFC_NETBANKING_URL = 'https://netbanking.hdfcbank.com/netbanking/';
+const HDFC_NETBANKING_URL = "https://netbanking.hdfcbank.com/netbanking/";
 
 const selectors = {
   login: {
-    FRAME: 'frame',
+    FRAME: "frame",
     CUSTOMER_ID_INPUT: 'input[name="fldLoginUserId"]',
     PASSWORD_INPUT: 'input[name="fldPassword"]',
     CONTINUE_BUTTON: 'img[alt="continue"]',
@@ -23,28 +23,29 @@ const selectors = {
   },
   statement: {
     LEFT_MENU_FRAME: 'frame[name="left_menu"]',
-    ACCT_SUMMARY_BTN: 'li.menu-summary.active a',
+    ACCT_SUMMARY_BTN: "li.menu-summary.active a",
     MAIN_PART_FRAME: 'frame[name="main_part"]',
-    VIEW_STATEMENT_BUTTON: 'a.viewbtngrey',
+    VIEW_STATEMENT_BUTTON: "a.viewbtngrey",
     WAIT_FOR_CHECK: 'form[name="frmTxn"]',
-    SAVINGS_ACCT_TABLE_HEADER: 'td.PSMSubHeader',
-  }
+    SAVINGS_ACCT_TABLE_HEADER: "td.PSMSubHeader",
+  },
 };
 
 async function login(config, page, retriesLeft = 3) {
   if (!retriesLeft) return false;
-  console.log('Logging in...', `[${4 - retriesLeft} / 3]`);
+  console.log("Logging in...", `[${4 - retriesLeft} / 3]`);
   try {
-    await page.goto(HDFC_NETBANKING_URL, {waitUntil: 'networkidle2'});
+    await page.goto(HDFC_NETBANKING_URL, { waitUntil: "networkidle2" });
     const frameElement = await page.waitForSelector(selectors.login.FRAME);
     const frame = await frameElement.contentFrame();
     await frame.type(selectors.login.CUSTOMER_ID_INPUT, config.customerId);
     await frame.click(selectors.login.CONTINUE_BUTTON);
     await frame.waitForSelector(selectors.login.PASSWORD_INPUT);
     await frame.type(selectors.login.PASSWORD_INPUT, config.password);
-    if (config.secureAccess) await frame.click(selectors.login.SECURE_ACCESS_CHECKBOX);
+    if (config.secureAccess)
+      await frame.click(selectors.login.SECURE_ACCESS_CHECKBOX);
     await frame.click(selectors.login.LOGIN_BUTTON);
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
     return true;
   } catch (err) {
     await wait(1000);
@@ -53,12 +54,12 @@ async function login(config, page, retriesLeft = 3) {
 }
 
 async function logout(page) {
-  console.log('Logging out...');
+  console.log("Logging out...");
   try {
     const frameElement = await page.waitForSelector(selectors.logout.FRAME);
     const frame = await frameElement.contentFrame();
     await frame.click(selectors.logout.LOGOUT_BUTTON);
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
   } catch (err) {
     await wait(1000);
     logout(page);
@@ -67,14 +68,20 @@ async function logout(page) {
 
 async function openStatement(config, page, retriesLeft, index) {
   if (!retriesLeft) return false;
-  console.log('Opening statement...', `[${4 - retriesLeft} / 3]`);
+  console.log("Opening statement...", `[${4 - retriesLeft} / 3]`);
   try {
-    const mainPartFrameElement = await page.waitForSelector(selectors.statement.MAIN_PART_FRAME);
+    const mainPartFrameElement = await page.waitForSelector(
+      selectors.statement.MAIN_PART_FRAME
+    );
     const mainPartFrame = await mainPartFrameElement.contentFrame();
     await mainPartFrame.click(selectors.statement.SAVINGS_ACCT_TABLE_HEADER);
-    const viewStatementBtn = (await mainPartFrame.$$(selectors.statement.VIEW_STATEMENT_BUTTON))[index];
+    const viewStatementBtn = (
+      await mainPartFrame.$$(selectors.statement.VIEW_STATEMENT_BUTTON)
+    )[index];
     await viewStatementBtn.click();
-    const form = await mainPartFrame.waitForSelector(selectors.statement.WAIT_FOR_CHECK);
+    const form = await mainPartFrame.waitForSelector(
+      selectors.statement.WAIT_FOR_CHECK
+    );
     return form;
   } catch (err) {
     await wait(1000);
@@ -83,7 +90,7 @@ async function openStatement(config, page, retriesLeft, index) {
 }
 
 async function getBalance(balanceTable) {
-  const balanceElement = await balanceTable.$('b');
+  const balanceElement = await balanceTable.$("b");
   const balanceText = await innerText(balanceElement);
   const balance = Number(amountToDecimal(balanceText.slice(4)));
   return balance;
@@ -91,9 +98,9 @@ async function getBalance(balanceTable) {
 
 async function getTransactions(transactionsTable) {
   const transactions = [];
-  const rows = (await transactionsTable.$$('tr')).slice(1);
+  const rows = (await transactionsTable.$$("tr")).slice(1);
   for (const row of rows) {
-    const cells = await Promise.all((await row.$$('td')).map(innerText));
+    const cells = await Promise.all((await row.$$("td")).map(innerText));
     transactions.push({
       id: cells[2],
       description: cells[1],
@@ -102,42 +109,49 @@ async function getTransactions(transactionsTable) {
       withdrawal: amountToDecimal(cells[4]),
       deposit: amountToDecimal(cells[5]),
       closingBalance: amountToDecimal(cells[6]),
-    })
+    });
   }
   return transactions;
 }
 
 async function parseStatement(form) {
-  console.log('Parsing statement...');
-  const tables = await form.$$('table');
+  console.log("Parsing statement...");
+  const tables = await form.$$("table");
   const balance = await getBalance(tables[1]);
   const transactions = await getTransactions(tables[2]);
-  return {balance, transactions};
+  return { balance, transactions };
 }
 
 async function launchBrowserAndLogin(config) {
-  console.log('Launching browser...');
-  const browser = await pptr.launch({headless: config.headless, args: ['--no-sandbox']});
+  console.log("Launching browser...");
+  const browser = await pptr.launch({
+    headless: config.headless,
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
-  await page.setViewport({width: 1200, height: 720});
+  await page.setViewport({ width: 1200, height: 720 });
   if (!(await login(config, page))) return false;
   return [browser, page];
 }
 
 async function logoutAndCloseBrowser(browser, page) {
   await logout(page);
-  console.log('Closing browser...');
+  console.log("Closing browser...");
   await page.close();
   await browser.close();
 }
 
 async function goBackToAccountSummary(page, retriesLeft = 3) {
   if (!retriesLeft) return false;
-  console.log('Going back to accounts page...', `[${4 - retriesLeft} / 3]`);
+  console.log("Going back to accounts page...", `[${4 - retriesLeft} / 3]`);
   try {
-    const leftMenuFrameElement = await page.waitForSelector(selectors.statement.LEFT_MENU_FRAME);
+    const leftMenuFrameElement = await page.waitForSelector(
+      selectors.statement.LEFT_MENU_FRAME
+    );
     const leftMenuFrame = await leftMenuFrameElement.contentFrame();
-    const accountSummaryBtn = await leftMenuFrame.$(selectors.statement.ACCT_SUMMARY_BTN);
+    const accountSummaryBtn = await leftMenuFrame.$(
+      selectors.statement.ACCT_SUMMARY_BTN
+    );
     await accountSummaryBtn.click();
   } catch (err) {
     return goBackToAccountSummary(page, --retriesLeft);
